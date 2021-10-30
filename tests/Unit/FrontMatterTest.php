abd4a836e91bc68c13fc042c191ec2c074197bc3
Daniel Seuffer <authanram@gmail.com>
2021-10-31 01:13:33 +0200

    WIP

--- tests/Unit/FrontMatterTest.php
+++ tests/Unit/FrontMatterTest.php
@@ -2,10 +2,27 @@
 
 declare(strict_types=1);
 
+$data = [
+    'index' => [
+        'First' => [
+            'first-link' => 'First Link',
+            'second-link' => 'Second Link',
+        ],
+        'Second' => [
+            'third-link' => 'Third Link',
+            'fourth-link' => 'Fourth Link',
+        ],
+    ],
+];
+
 beforeEach(function () {
     $this->converter = converter('front-matter');
 });
 
-it('parses front matter', function () {
-    expect(true)->toBeTrue();
+it('parses front matter', function () use ($data) {
+    expect($this->converter->getFrontMatter())->toEqual($data);
+});
+
+it('parses front matter index', function () use ($data) {
+    expect($this->converter->getIndex())->toEqual($data['index']);
 });
